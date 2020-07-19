f03c12103e2d1260feffc60c7b7a881612cab39e
Danny August Ramaputra <daystram@gmail.com>
2020-07-19 23:12:53 +1000

    Composed home page

--- folio-fe/pages/index.js
+++ folio-fe/pages/index.js
@@ -1,209 +1,78 @@
 import Head from 'next/head'
+import {useEffect, useState} from "react";
+import {motion, useTransform, useViewportScroll} from "framer-motion";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faGithub, faGitlab, faInstagram, faLinkedinIn, faMediumM, faTwitter} from '@fortawesome/free-brands-svg-icons'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import styles from './index.module.sass'
+import Logo from "../components/logo";
+import Chevron from '../components/chevron';
 
 export default function Home() {
+  const [scrolling, setScrolling] = useState(false);
+  const [atStart, setAtStart] = useState(true);
+  const {scrollY} = useViewportScroll();
+  const window_height = process.browser ? window.innerHeight : 0;
+  const title_height = useTransform(scrollY, [0, window_height - 48], [window_height, 48]);
+  const name_scale = useTransform(scrollY, [0, window_height - 48], [1, 0.4]);
+  const logo_dimension = useTransform(scrollY, [0, window_height - 48], [112, 32]);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScrolling(scrollY.current > window_height - 48);
+      setAtStart(scrollY.current === 0);
+    })
+  })
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Danny August Ramaputra</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
       </Head>
-
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <section className={`${styles.head}`}>
+          <motion.div layout className={styles.title} data-isscrolling={scrolling} style={{height: title_height}}>
+            <motion.div layout style={{width: logo_dimension, height: logo_dimension,}}><Logo/></motion.div>
+            <motion.div layout>
+              <motion.h1
+                className={styles.name}
+                style={{scale: name_scale, transformOrigin: "left center"}}
+                animate={{opacity: [0, 1]}}
+                transition={{opacity: {duration: 2, ease: "easeInOut"}}}>
+                Danny August Ramaputra
+              </motion.h1>
+            </motion.div>
+            <motion.div layout className={styles.mediaGroup} data-isscrolling={scrolling}>
+              <a target="_blank" href="https://gitlab.com/daystram">
+                <FontAwesomeIcon icon={faGitlab} className={styles.mediaGlyph}/></a>
+              <a target="_blank" href="https://github.com/daystram">
+                <FontAwesomeIcon icon={faGithub} className={styles.mediaGlyph}/></a>
+              <a target="_blank" href="https://www.linkedin.com/in/daystram/">
+                <FontAwesomeIcon icon={faLinkedinIn} className={styles.mediaGlyph}/></a>
+              <a target="_blank" href="https://twitter.com/daystram">
+                <FontAwesomeIcon icon={faTwitter} className={styles.mediaGlyph}/></a>
+              <a target="_blank" href="https://www.instagram.com/daystram_/">
+                <FontAwesomeIcon icon={faInstagram} className={styles.mediaGlyph}/></a>
+              <a target="_blank" href="https://medium.com/@daystram">
+                <FontAwesomeIcon icon={faMediumM} className={styles.mediaGlyph}/></a>
+              <a target="_blank" href="mailto:daystram@gmail.com">
+                <FontAwesomeIcon icon={faEnvelope} className={styles.mediaGlyph}/></a>
+            </motion.div>
+          </motion.div>
+          <div className={styles.chevronContainer}><Chevron shown={atStart}/></div>
+        </section>
+        <section className={`${styles.section} ${styles.sectionEven}`}>
+          <h2 className={styles.heading}>good content</h2>
+        </section>
+        <section className={`${styles.section} ${styles.sectionOdd}`}>
+          <h2 className={styles.heading}>better content</h2>
+        </section>
+        <footer className={styles.footer}>
+          <div className={styles.copy}>Danny August Ramaputra {new Date().getFullYear()}</div>
+        </footer>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
