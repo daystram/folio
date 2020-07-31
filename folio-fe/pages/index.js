bc47e49a2debf87b7571b2b3a526580d288ce52e
Danny August Ramaputra <daystram@gmail.com>
2020-08-01 08:50:10 +1000

    Added education, experience sections

--- folio-fe/pages/index.js
+++ folio-fe/pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
               <motion.h1
                 className={styles.name}
                 style={{scale: name_scale, transformOrigin: "left center"}}
-                animate={{opacity: [0, 1]}}
+                animate={{opacity: [0, 1], color: ["#ededed00", "#edededff"]}}
                 transition={{opacity: {duration: 2, ease: "easeInOut"}}}>
                 Danny August Ramaputra
               </motion.h1>
@@ -67,29 +67,136 @@ export default function Home() {
           <div className={"container mx-auto"}>
             <h2 className={styles.sectionHeading}>Education</h2>
             <div className={styles.entry}>
-              <img src={"android-chrome-192x192.png"} width={64}/>
+              <img src={"logo-uq.png"} className={styles.entryLogo}/>
               <div className={styles.entryContainer}>
-                <h3 className={styles.entryHeading}>University of Queensland</h3>
-                <p>2020 - present</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt eum laboriosam, odit
-                  quibusdam quidem quisquam sapiente voluptas voluptate voluptates. Distinctio exercitationem fugit
-                  itaque nam non repellat sapiente vel voluptate.</p>
+                <div className={styles.entryHeadContainer}>
+                  <h3 className={styles.entryHeading1}>The University of Queensland</h3>
+                  <h4 className={styles.entryHeading2}>Bachelor of Computer Science, GPA: 7.00/7.00</h4>
+                </div>
+                <div className={styles.entryBodyContainer}>
+                  <p className={styles.entryHeadingSub1}>Feb 2020 – present</p>
+                  <p className={styles.entryHeadingSub1}>Brisbane, QLD, Australia</p>
+                  <ul className={"list-disc"} style={{paddingLeft: 20}}>
+                    <li>Majoring in data science</li>
+                    <li>Scholarship recipient for outstanding academic achievements</li>
+                    <li>UQ Computing Society member</li>
+                  </ul>
+                </div>
               </div>
             </div>
-            <div>
-              <h3>Universitas Indonesia</h3>
-              <p>2017 - present</p>
+            <div className={styles.entry}>
+              <img src={"logo-ui.png"} className={styles.entryLogo}/>
+              <div className={styles.entryContainer}>
+                <div className={styles.entryHeadContainer}>
+                  <h3 className={styles.entryHeading1}>Universitas Indonesia</h3>
+                  <h4 className={styles.entryHeading2}>Bachelor of Computer Science, GPA: 3.99/4.00</h4>
+                </div>
+                <div className={styles.entryBodyContainer}>
+                  <p className={styles.entryHeadingSub1}>Jul 2017 – present</p>
+                  <p className={styles.entryHeadingSub1}>Jakarta, Indonesia</p>
+                  <ul className={"list-disc"} style={{paddingLeft: 20}}>
+                    <li>Double degree also in Computer Science with The University of Queensland</li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </section>
         <section className={`${styles.section} ${styles.sectionOdd}`}>
-          <h2 className={styles.heading}>Experience</h2>
-        </section>
-        <section className={`${styles.section} ${styles.sectionEven}`}>
-          <h2 className={styles.heading}>Skills</h2>
-        </section>
-        <section className={`${styles.section} ${styles.sectionOdd}`}>
-          <h2 className={styles.heading}>Projects</h2>
+          <div className={"container mx-auto"}>
+            <h2 className={styles.sectionHeading}>Experience</h2>
+            <div className={styles.entry}>
+              <img src={"logo-hf.png"} className={styles.entryLogo}/>
+              <div className={styles.entryContainer}>
+                <div className={styles.entryHeadContainer}>
+                  <h3 className={styles.entryHeading1}>Software Developer</h3>
+                  <h4 className={styles.entryHeading2}>HappyFresh, Part-time</h4>
+                </div>
+                <div className={styles.entryBodyContainer}>
+                  <p className={styles.entryHeadingSub1}>Sep 2019 – Dec 2019, 4 months</p>
+                  <p className={styles.entryHeadingSub1}>Jakarta, Indonesia</p>
+                  <ul className={"list-disc"} style={{paddingLeft: 20}}>
+                    <li>A part of a course in Universitas Indonesia</li>
+                    <li>Developed Fleet Tracker service to track customer orders in real-time</li>
+                    <li>Advanced coordinate interpolation with Kalman filters are utilized to help reduce GPS usage load on the courier's mobile devices</li>
+                    <li>Location stream retrieved from ActiveMQ managed by HappyFresh</li>
+                    <li>Golang back-end and JS/React front-end with WebSocket communication</li>
+                    <li>Automated deployment using Docker containers to AWS EC2 via GitLab CI</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className={styles.entry}>
+              <img src={"logo-sp.png"} className={styles.entryLogo}/>
+              <div className={styles.entryContainer}>
+                <div className={styles.entryHeadContainer}>
+                  <h3 className={styles.entryHeading1}>Back-end Developer</h3>
+                  <h4 className={styles.entryHeading2}>Shopee, Full-time Intern</h4>
+                </div>
+                <div className={styles.entryBodyContainer}>
+                  <p className={styles.entryHeadingSub1}>May 2019 – Aug 2019, 4 months</p>
+                  <p className={styles.entryHeadingSub1}>Singapore</p>
+                  <ul className={"list-disc"} style={{paddingLeft: 20}}>
+                    <li>Developed an adaptive versioning deployment system for Shopee's website across 7 countries, integrating with the on-premise GitLab</li>
+                    <li>Developed an automatic remote profiling system for the Golang-based services</li>
+                    <li>Developed a voting system for use within the mobile application to support tenants' requirements</li>
+                    <li>Mostly developed using Golang back-end, automatically deployed into Kubernetes cluster</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className={styles.entry}>
+              <img src={"logo-pk.png"} className={styles.entryLogo}/>
+              <div className={styles.entryContainer}>
+                <div className={styles.entryHeadContainer}>
+                  <h3 className={styles.entryHeading1}>Software Engineer</h3>
+                  <h4 className={styles.entryHeading2}>Pusat Ilmu Komputer Universitas Indonesia, Part-time</h4>
+                </div>
+                <div className={styles.entryBodyContainer}>
+                  <p className={styles.entryHeadingSub1}>Mar 2019 – Aug 2019</p>
+                  <p className={styles.entryHeadingSub1}>Jakarta, Indonesia</p>
+                  <ul className={"list-disc"} style={{paddingLeft: 20}}>
+                    <li>Developed a mobile application for 'Quality in Research International Conference'</li>
+                    <li>Developed a mobile application for 'International Tropical Renewable Energy Conference'</li>
+                    <li>Mobile application is powered with Google Firestore real-time database for data storage and live chat</li>
+                    <li>NodeJS back-end and React Native-based cross-platform front-end</li>
+                    <li>Back-end uses WebSocket communication acting as a buffer between user mobile chat clients, the customer service client, and Google Firestore to offload client load and optimize read/write operations on Firestore</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className={styles.entry}>
+              <img src={"logo-ui.png"} className={styles.entryLogo}/>
+              <div className={styles.entryContainer}>
+                <div className={styles.entryHeadContainer}>
+                  <h3 className={styles.entryHeading1}>Teaching Assistant Coordinator</h3>
+                  <h4 className={styles.entryHeading2}>Universitas Indonesia</h4>
+                </div>
+                <div className={styles.entryBodyContainer}>
+                  <p className={styles.entryHeadingSub1}>Sep 2018 – Jun 2019, 10 months</p>
+                  <p className={styles.entryHeadingSub1}>Jakarta, Indonesia</p>
+                  <ul className={"list-disc"} style={{paddingLeft: 20}}>
+                    <li>(Jan 2019 - Jun 2019, 6 months) Teaching assistant for 'Programming Foundations 2' course</li>
+                    <li>(Jan 2019 - Jun 2019, 6 months) Teaching assistant coordinator for 'Introduction to Computer Organization' course</li>
+                    <li>(Sep 2018 - Jan 2019, 5 months) Teaching assistant coordinator for 'Introduction to Digital Systems' course</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className={styles.entry}>
+              <img src={"logo-trui.png"} className={styles.entryLogo}/>
+              <div className={styles.entryContainer}>
+                <div className={styles.entryHeadContainer}>
+                  <h3 className={styles.entryHeading1}>Autonomous Rocket Research System Programmer</h3>
+                  <h4 className={styles.entryHeading2}>Tim Robotika Universitas Indonesia</h4>
+                </div>
+                <div className={styles.entryBodyContainer}>
+                  <p className={styles.entryHeadingSub1}>Mar 2018 – Jun 2019, 1 year 4 months</p>
+                  <p className={styles.entryHeadingSub1}>Jakarta, Indonesia</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
         <footer className={styles.footer}>
           <div className={styles.copy}>Danny August Ramaputra {new Date().getFullYear()}</div>
