5874cb08588ac65390c77ef56869edfac0c67e2e
Danny August Ramaputra <daystram@gmail.com>
2021-02-18 05:05:00 +1000

    refactor: updated footer

--- folio-fe/components/FooterBar.tsx
+++ folio-fe/components/FooterBar.tsx
@@ -6,6 +6,9 @@ export default function FooterBar() {
   return (
     <footer className={styles.footer}>
       <div className={styles.copy}>
+        Self-Hosted at K8s Cluster
+        <br />
+        <br />
         Danny August Ramaputra {new Date().getFullYear()}
       </div>
     </footer>
